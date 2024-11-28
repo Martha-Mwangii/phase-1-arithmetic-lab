c5a14a0fd95a474a340725b0ba3d1334d9e62c5c
Martha Mwangii <marthawm7252@gmail.com>
2024-11-28 20:36:18 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+const multiply = num2*num1;
+console.log(multiply)
+
+
+let random = Math.floor(Math.random()*10)+1
+//console.log(random)
+
+
+let num3=9
+let num4=5
+
+const mod = num3%num4
+console.log(mod)
+
+
+let max=Math.max(1,4,20,18,7);
+console.log(max)
